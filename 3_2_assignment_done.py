229f93d4c1672448111d82382cf2e7f2221a1931
x20c <vbachinin2018@gmail.com>
2025-02-12 00:18:16 +0200

    Update 3_2_assignment_done.py

--- 3_2_assignment_done.py
+++ 3_2_assignment_done.py
@@ -34,30 +34,22 @@ print(find_primes_2(1, 11))  # should print [2, 3, 5, 7, 11]
 print('Task 3')
 list_in = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-list_out = list_in[1:10:2]
+list_out = list_in[1:len(list_in)+1:2]
 
 print(list_out)  # should print [1, 3, 5, 7, 9]
 
 #Task 4
 print('Task 4')
 def is_even(num):
-    lis = []
-    lis2 = []
-    for i in range(1, len(num)+1):
-        if i % 2 == 0:
-            lis.append(i)
-        else:
-            lis2.append(i)
-    unsorted_list.clear()
-    lis_done = lis + lis2
-    for i in lis_done:
-        unsorted_list.append(i)
+    return num % 2 == 0
+def sort(num):
+    return not is_even(num)
 
 unsorted_list = [1, 2, 3, 4, 5, 6, 7]
 
-is_even(unsorted_list)
+sorted_list = sorted(unsorted_list, key=sort)
 
-print(unsorted_list)  # should print [2, 4, 6, 1, 3, 5, 7] or any other valid sorting
+print(sorted_list)  # should print [2, 4, 6, 1, 3, 5, 7] or any other valid sorting
 
 #Task 5
 print('Task 5')
