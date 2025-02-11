3823be3424458dafe9bdf97774ed7d02ab7f6ad3
x20c <vbachinin2018@gmail.com>
2025-02-12 00:02:31 +0200

    Update cesar.py

--- cesar.py
+++ cesar.py
@@ -1,21 +1,28 @@
 #crypt.py
 def crypt():
     key = int(input('Enter your key: '))
-    inp = input("Enter a world would you like to crypt: ")
+    inp = input("Enter a word you would like to crypt: ")
     crypted = []
-    for i in range(len(inp)):
-        crypted.append(ord(inp[i]) + key)
-    return crypted
+    for char in inp:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            new_char = chr((ord(char) - base + key) % 26 + base)
+        else:
+            new_char = char
+        crypted.append(new_char)
+    return ''.join(crypted)
 print(crypt())
 #decrypt.py
 def decrypt():
     key = int(input('Enter your key: '))
-    inp = input("Enter a numbers with comma: ")
-    crypted = []
-    for i in inp.split(','):
-        crypted.append(int(i))
-    decrypted = ''
-    for i in range(len(crypted)):
-        decrypted += chr(crypted[i] - key)
-    return decrypted
+    inp = input("Enter a word to decrypt: ")
+    decrypted = []
+    for char in inp:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            new_char = chr((ord(char) - base - key) % 26 + base)
+        else:
+            new_char = char
+        decrypted.append(new_char)
+    return ''.join(decrypted)
 print(decrypt())
