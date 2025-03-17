caa01ccc69cad0654aff027f4248a573e86b6c01
x20c <vbachinin2018@gmail.com>
2025-03-17 13:26:51 +0200

    Update 8_2_done.py

--- 8_2_done.py
+++ 8_2_done.py
@@ -1,36 +1,36 @@
 #Task 1
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 
-class Account:
-    def __init__(self, number_acc : int, name : str, balance : float):
+class Account(ABC):
+    def __init__(self, number_acc: int, name: str, balance: float):
         self.number_acc = number_acc
         self.name = name
         self.balance = balance
 
-    def deposit(self, deposit : float):
-        if deposit > 0:
-            self.balance += deposit
-            print(f"Successfully deposited {deposit} to {self.name}, current balance is {self.balance}")
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.balance += amount
+            print(f"Successfully deposited {amount} to {self.name}, current balance is {self.balance}")
         else:
             raise ValueError("Deposit can't be negative")
 
-    def withdraw(self, withdraw : float):
-        if withdraw <= 0:
+    def withdraw(self, amount: float):
+        if amount <= 0:
             raise ValueError("Withdraw must be a positive number")
-        if withdraw <= self.balance:
-            self.balance -= withdraw
+        if amount <= self.balance:
+            self.balance -= amount
             print(f"Withdraw successful. Balance is {self.balance}")
         else:
             raise ValueError(f"Withdraw must be lower than balance, current balance is {self.balance}")
 
     @abstractmethod
     def __str__(self):
-      raise NotImplementedError("Subclasses must implement __str__ method")
+        pass
 #Task 2
 class CurrentAccount(Account):
     def __str__(self):
-        return f"Current account is {self.number_acc}, current balance is {self.balance}"
+        return f"Current Account - {self.name} (#{self.number_acc}), Balance: {self.balance}"
 #Task 3
 class SavingsAccount(Account):
     def __init__(self, number_acc: int, name: str, balance: float, interest_rate: float):
@@ -38,11 +38,12 @@ class SavingsAccount(Account):
         self.interest_rate = interest_rate
 
     def add_interest(self):
-        self.balance += self.balance * self.interest_rate
-        print(f"Added interest rate of {self.balance} rate is {self.interest_rate}")
+        interest = self.balance * self.interest_rate
+        self.balance += interest
+        print(f"Added interest: {interest:.2f}, new balance is {self.balance:.2f}")
 
     def __str__(self):
-        return f"Savings Account: {self.number_acc}, Interest Rate: {self.interest_rate * 100:.2f}%, Balance: {self.balance}"
+        return f"Savings Account - {self.name} (#{self.number_acc}), Interest Rate: {self.interest_rate * 100:.2f}%, Balance: {self.balance:.2f}"
 #Task 4
 accounts = [
     CurrentAccount(12345, "Adolf Gitler", 9999999999.0),
